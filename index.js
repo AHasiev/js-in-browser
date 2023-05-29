762b4abd501ed73573ea316171e3684f61dc138a
ahasiev <ahasiev@mail.ru>
2023-05-29 11:53:28 +0300

    сделал стрелку

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function readMore() {
   const more = document.getElementById("more");
   const btn = document.getElementById("btn");
+  const imgDown = document.getElementById("imgDown");
 
   if (btn.innerHTML === "Показать все") {
     btn.innerHTML = "Скрыть";
@@ -9,14 +10,14 @@ function readMore() {
     btn.innerHTML = "Показать все";
     more.style.display = "none";
   }
+
+  imgDown.classList.toggle("imgUp");
 }
 
-var swiper = new Swiper('.swiper-container', {
-    
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1.15,
   pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-    
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
-
